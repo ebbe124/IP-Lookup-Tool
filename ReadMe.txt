549f2a807f0dfb1df5ed0bb60d84684b9e6d4ab5
ebbe124 <81823893+ebbe124@users.noreply.github.com>
2021-04-19 08:11:28 -0700

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,16 @@
+# An IP Lookup Made By EbBe#9999
+#Support Me:
+Discord Server:https://www.paypal.me/hgdhgd0011
+PayPal: https://www.paypal.me/hgdhgd0011
+#Contact Me:
+Discord: EbBe#9999
+Mail:ebbey365@gmail.com
+
+#Facts
+Coded In Visual Studios Code
+Launage: Python
+
+#Credits
+No One I Made It Myself
+
+Thank You For Using My Tool! I Hope You Liked It!
---
+++ main.py
@@ -0,0 +1,32 @@
+print ("")
+print ("")
+print ("|_ _| |  _ \    | |       ___     ___   | | __  _   _   _ __     | __ )   _   _    | ____| | |__   | __ )    ___   _| || |_   / _ \   / _ \   / _ \   / _ \ ")
+print ("| |  | |_) |   | |      / _ \   / _ \  | |/ / | | | | | '_ \    |  _ \  | | | |   |  _|   | '_ \  |  _ \   / _ \ |_  ..  _| | (_) | | (_) | | (_) | | (_) |")
+print ("| |  |  __/    | |___  | (_) | | (_) | |   <  | |_| | | |_) |   | |_) | | |_| |   | |___  | |_) | | |_) | |  __/ |_      _|  \__, |  \__, |  \__, |  \__, |")
+print ("|___| |_|       |_____|  \___/   \___/  |_|\_\  \__,_| | .__/    |____/   \__, |   |_____| |_.__/  |____/   \___|  |_||_|      /_/     /_/     /_/     /_/ ")
+print ("                                                       |_|                |___/                                                                        ")     
+
+print ("Only you Are Responsable For Your Actions")
+print ("Support Me")
+print ("PayPalhttpswww.paypal.me/hgdhgd0011")
+print ("Discord Server:discord.ggDEQDmnVrbQ")
+import requests
+while True:
+    try:
+        ip = input("Enter Ip: ")  
+        Get = requests.get(f"http://ip-api.com/json/{ip}?fields=66842623")
+        Get_values = Get.json()
+        if Get_values['status'] == "fail":
+            wowowow = Get_values['message']
+            print(f"Operation failed. Reason: {wowowow}")
+        else:
+            for i in Get_values:
+                x = Get_values[i]
+                while len(i) < 13: # this is only for like space injection
+                    i = " " + i
+                if x == "":
+                    x = "Undefined"
+                print(i,":", x)
+    except:
+        wowowow = "Connection issue, check internet"
+        print(f"Operation failed. Reason: {wowowow}")
